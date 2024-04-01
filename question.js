83779b4aa5c3d5e16338dc278e8d4cfa1ae14c9b
PreetiDiwakarGitHub <p208016@gmail.com>
2024-04-01 10:03:52 +0530

    question

---
+++ question.js
@@ -0,0 +1,98 @@
+// let str="Hello world, welcome to the universe";
+// console.log(str.indexOf("Hello"));
+
+
+// let str="this is string";
+// if(str.indexOf("This")!==-1){
+//     console.log("Found");
+// }
+
+// include string
+
+// let str="hello";
+// console.log(str.includes("h"));    ture
+
+// let str="This is string";
+// console.log(str.includes("this"));     falue
+
+
+// let str="Hello";
+// console.log(str.startsWith("H"));        Ture
+
+// let str="This is string";
+// console.log(str.endsWith("g"));
+
+// Slice
+
+// let str="this is a string";
+// console.log(str.slice(0,3));            Thi
+
+// let str="this is a string";
+//  console.log(str.slice(5,8)); 
+
+
+// let str="this is a string";
+//  console.log(str.slice(0)); 
+
+// let str="this is a string";
+// console.log(str.slice(-6));
+
+// substring
+
+// let str="this is a string";
+// console.log(str.substring(0,3));
+
+// let str="this is a string";
+// console.log(str.substring(3,0));
+
+// let str="this is a string";
+//  console.log(str.substring(-4,3));
+
+
+// Trin
+
+// let str="     this is a string    ";
+//  console.log(str.trim());
+
+
+// let str="     this is a string    ";
+//  console.log(str.trimStart());
+//  console.log(str.trimEnd());
+
+// padstart
+
+// let str="Hello";
+// console.log(str.padStart(7,"o"));
+
+// padend
+// let str="Hello";
+// console.log(str.padEnd(7,"o"));
+
+// Repeat
+
+// let str="Hello";
+// console.log(str.repeat(3));
+
+//  let str="Hello";
+//  console.log(str.repeat(0));
+// Replace
+
+// let str="This is a string";
+// console.log(str.replace("string","code"));
+
+// let str="This is a string string";
+//  console.log(str.replace("string","code"));
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/PreetiDiwakarGitHub/practice.git
+// git push -u origin main
+
+
+
+
+
+
